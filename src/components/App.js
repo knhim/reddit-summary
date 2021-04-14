fd12d6c43a32f2a38b08966b66d3330f6e9c5164
Kevin Nhim <nhimkap4@gmail.com>
2021-04-13 21:02:59 -0700

    updated searchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 
 //fetch example (reddit.com/r/manga/hot.json)
 
-const fetchData = async () => {
-  try {
-    const response = await axios.get('https://www.reddit.com/r/manga/hot.json');
-    console.log(response.data.data.children[1].data.title);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 const App = () => {
-  fetchData();
+  const [data, setData] = useState({});
+
+  // useEffect(async () => {
+  //   try {
+  //     const response = await axios.get('https://www.reddit.com/r/manga/hot.json');
+  //     setData(response);
+  //     console.log(response.data.data.children[1].data.title);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }, []);
+
   return (
     <>
       <h1>Home Page</h1>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
+//${query}/hot.json
 const SearchBar = () => {
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('');
+  const [url, setUrl] = useState(`https://www.reddit.com/r/frontpage/hot.json`);
+
+  const handleClick = e => {
+    e.preventDefault();
+    setUrl(`https://www.reddit.com/r/${query}/hot.json`);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        console.log(response.data);
+        setData(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, [url]);
+
   return (
     <>
-      <input type="text" className="border-2 border-grey-500" />
+      <form onSubmit={() => handleClick()}>
+        <label htmlFor="search">Search Subreddits</label>
+        <input
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+          type="text"
+          className="border-2 border-grey-500"
+        />
+        <button type="button">Search</button>
+      </form>
     </>
   );
 };
+
 export default SearchBar;
