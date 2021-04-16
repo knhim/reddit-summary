bdd01917f262721feee393296131e9bd8d6d59c4
knhim <nhimkap4@gmail.com>
2021-04-15 21:40:38 -0700

    added a class implementation to test out wrapper

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import SearchBar from './SearchBar';
 import Posts from './Posts';
 require('dotenv').config();
 const snoowrap = require('snoowrap');
 // import IndividualPost from './IndividualPost';
-console.log(process.env.REACT_APP_CLIENT_ID);
+
 // fetch example (reddit.com/r/manga/hot.json)
 const r = new snoowrap({
   userAgent: process.env.REACT_APP_USER_AGENT,
@@ -14,40 +14,66 @@ const r = new snoowrap({
   refreshToken: process.env.REACT_APP_REFRESH_TOKEN,
 });
 
-const App = () => {
-  const [data, setData] = useState({ hits: [] });
-  const [query, setQuery] = useState('');
-  const [url, setUrl] = useState(`https://www.reddit.com/r/all/hot.json`);
-  //show hot threads from this subreddit
-  // r.getHot('manga');
-  // grab comments from a thread
-  r.getSubmission('mqy9fz').expandReplies({ limit: 1, depth: 1 }).then(console.log);
+// const App = () => {
+//   const [data, setData] = useState({ hits: [] });
+//   const [query, setQuery] = useState('');
+//   const [url, setUrl] = useState(`https://www.reddit.com/r/all/hot.json`);
+//   //show hot threads from this subreddit
+//   r.getHot('manga').then(console.log);
 
-  const handleClick = e => {
-    e.preventDefault();
-    setUrl(`https://www.reddit.com/r/${query}/hot.json`);
+//   const handleClick = e => {
+//     e.preventDefault();
+//     setUrl(`https://www.reddit.com/r/${query}/hot.json`);
+//   };
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await r.getHot('manga');
+//         setData(response);
+//       } catch (err) {
+//         console.error(err);
+//       }
+//     };
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <>
+//       <h1 className="flex justify-center">Home Page</h1>
+//       {/* <SearchBar query={query} setQuery={setQuery} handleClick={handleClick} />
+//       <Posts data={data} /> */}
+//       {/* <IndividualPost data={data} /> */}
+//     </>
+//   );
+// };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      data2: [],
+    };
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  fetchData = async () => {
+    try {
+      const response = await r.getHot('manga');
+      this.setState({ data: [response] });
+    } catch (err) {
+      console.error(err);
+    }
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(url);
-        setData(response.data.data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    fetchData();
-  }, [url]);
+  componentDidMount() {
+    this.fetchData();
+  }
 
-  return (
-    <>
-      <h1 className="flex justify-center">Home Page</h1>
-      <SearchBar query={query} setQuery={setQuery} handleClick={handleClick} />
-      <Posts data={data} />
-      {/* <IndividualPost data={data} /> */}
-    </>
-  );
-};
+  render() {
+    return <h1>hello</h1>;
+  }
+}
 
 export default App;
