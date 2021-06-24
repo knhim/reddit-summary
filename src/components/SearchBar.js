66f9c2ddc05a6f561a99b049b506f9fbeab5b6d4
knhim <nhimkap4@gmail.com>
2021-06-24 16:58:47 -0700

    styled search bar and form element

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -25,19 +25,15 @@ const SearchBar = (props) => {
 
   return (
     <>
-      <form className="flex flex-col justify-center mt-1 mb-1" onSubmit={props.handleSearch}>
-        <label className="mr-2" htmlFor="search">
-          Search Subreddits
-        </label>
-        <div>
-          <input
-            value={props.query}
-            onChange={(e) => props.setQuery(e.target.value)}
-            type="text"
-            className="border-2 border-grey-500 w-6/12"
-          />
-          <button type="submit">Search</button>
-        </div>
+      <form className="flex flex-col w-8/12 m-auto my-2" onSubmit={props.handleSearch}>
+        <label htmlFor="search">Search Subreddit</label>
+        <input
+          value={props.query}
+          onChange={(e) => props.setQuery(e.target.value)}
+          type="text"
+          className="mr-2 border-2 border-grey-500"
+          placeholder="i.e. books"
+        />
       </form>
     </>
   );
